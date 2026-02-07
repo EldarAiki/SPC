4da489c87e446a1ca6aa9cf8887c5e4d6247ebe6
Eldar <loshinas@gmail.com>
2026-02-07 20:49:54 +0200

    runtime node

--- src/app/api/testdb/route.js
+++ src/app/api/testdb/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
+export const runtime = "nodejs";
+
+
 export async function GET() {
     try {
         const users = await prisma.user.findMany({ take: 10 });
